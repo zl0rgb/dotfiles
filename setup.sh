2e8a76d837ca4686fe4e98b09bc5be5a787d3461
zl0rgb <95036437+zl0rgb@users.noreply.github.com>
2024-04-09 12:10:28 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 #sudo pacman -S feh  picom rofi blueman-applet pa-applet xorg-xsetroot xorg-xrandr ttf-roboto ttf-roboto-mono-nerd
 mkdir -p $HOME/Pictures/wal/sol
 cp sol $HOME/Pictures/wal/sol
-cp .xprofile $HOME/.xprofile
-cp /roba\ da\ copiare/.vimrc $HOME/.vimrc
+cp xprofile $HOME/.xprofile
+cp /roba\ da\ copiare/vimrc $HOME/.vimrc
 cp a.sh $HOME/a.sh
 
 echo "done :3"
